9eb79116563eac74c38cb6428594a32fad9678ce
Dmitrii <Apocaliptic.love@gmail.com>
2023-01-25 17:33:20 +0400

    Add notNullValue

--- src/test/java/RestApiTests.java
+++ src/test/java/RestApiTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 
 public class RestApiTests {
@@ -68,8 +69,8 @@ public class RestApiTests {
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("id", is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("id", is(notNullValue()))
+                .body("token", is(notNullValue()));
     }
     @Test
     void deleteUserTest() {
